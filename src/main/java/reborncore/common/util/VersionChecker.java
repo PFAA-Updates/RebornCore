16a57f315a8c567eef8bcb655324d359bfc5bc8f
glowredman <fredcraft00@gmail.com>
2021-02-27 17:12:20 +0100

    Make VersionChecker deprecated

--- src/main/java/reborncore/common/util/VersionChecker.java
+++ src/main/java/reborncore/common/util/VersionChecker.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 
+@Deprecated
 public class VersionChecker {
 
     public static final String apiAddress = "http://modmuss50.me/api/v1/version.php";
